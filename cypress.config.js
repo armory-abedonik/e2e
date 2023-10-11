f028cf6e7993108e9314b3b617bc3b5118f2f3a7
Alexey Bedonik <alexey.bedonik@armory.io>
2023-10-11 18:42:20 +0200

    tmp

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true,
+  videoCompression: true,
   e2e: {
     supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       
       on('before:browser:launch', (browser = {}, launchOptions) => {
-        if (browser.family === 'chrome') {
-          launchOptions.args.push('--no-sandbox');
-          launchOptions.args.push('--disable-gpu');
-        }
-
-        if (browser.family === 'electron') {
-          launchOptions.devTools = false;
-        }
-
         return launchOptions;
       });
     },
